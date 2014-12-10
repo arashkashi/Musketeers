7f66d916dccb6554970e643c8eea6f55cde6e129
Arash Kashi <arashkashi@gmail.com>
2014-12-10 20:03:34 +0100

    added the game score view.

--- Musketeers/GameViewController.swift
+++ Musketeers/GameViewController.swift
@@ -9,26 +9,11 @@
 import UIKit
 import SpriteKit
 
-extension SKNode {
-    class func unarchiveFromFile(file : NSString) -> SKNode? {
-        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
-            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
-            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-            
-            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
-            archiver.finishDecoding()
-            return scene
-        } else {
-            return nil
-        }
-    }
-}
-
 class GameViewController: UIViewController {
 
     @IBOutlet var gameViewContainer: SKView!
     
+    // MARK: Swich between scenes
     func showGameScene() {
         if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
             // Configure the view.
@@ -45,6 +30,29 @@ class GameViewController: UIViewController {
             skView.presentScene(scene)
         }
     }
+    
+    func showScoreScene() {
+        if let scene = ScoreScene.unarchiveFromFileForGameScore("ScoreScene") as? ScoreScene {
+            // Configure the view.
+            let skView = self.gameViewContainer as SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+            
+            skView.presentScene(scene)
+        }
+    }
+    
+    // MARK: IB Actions
+    @IBAction func onSwitchSceneTapped(sender: AnyObject) {
+        showScoreScene()
+    }
+    
     
     // MARK: View Controller methods source
     override func viewDidLoad() {
---
+++ Musketeers/SKNodeExtension.swift
@@ -0,0 +1,42 @@
+//
+//  SKNodeExtension.swift
+//  Musketeers
+//
+//  Created by Arash Kashi on 10/12/14.
+//  Copyright (c) 2014 Wooga. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+
+extension SKNode {
+    class func unarchiveFromFile(file : NSString) -> SKNode? {
+        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
+            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
+            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+            
+            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
+            archiver.finishDecoding()
+            return scene
+        } else {
+            return nil
+        }
+    }
+    
+    class func unarchiveFromFileForGameScore(file : NSString) -> SKNode? {
+        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
+            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
+            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+            
+            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as ScoreScene
+            archiver.finishDecoding()
+            return scene
+        } else {
+            return nil
+        }
+    }
+    
+}
