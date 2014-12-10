8f5f4fd821f5e238bf3e38c6503c2d27a2d133ce
Arash Kashi <arashkashi@gmail.com>
2014-12-10 14:39:04 +0100

    made the jumping shit happening.

--- Musketeers/GameScene.swift
+++ Musketeers/GameScene.swift
@@ -10,36 +10,42 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
+        initFloor()
+        initMainPlayer()
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
+
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            var player = self.childNodeWithName("player")
+            player?.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 10))
         }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    
+    // MARK: Initiation
+    func initFloor() {
+        var floor = self.childNodeWithName("floor")
+        if let node = floor {
+            node.physicsBody = SKPhysicsBody(rectangleOfSize: node.frame.size)
+            node.physicsBody?.dynamic = false
+        } else {
+            assert(false, "Could not find the node")
+        }
+    }
+    
+    func initMainPlayer() {
+        var player = self.childNodeWithName("player")
+        if let node = player {
+            node.physicsBody = SKPhysicsBody(rectangleOfSize: node.frame.size)
+            node.physicsBody!.dynamic = true
+        } else {
+            assert(false, "coult not find the node")
+        }
     }
 }
