fa16c87cba6c7afc06f105f5dd7146bcfba747cb
Arash Kashi <arashkashi@gmail.com>
2014-12-11 17:57:13 +0100

    cleaned up the score scene

--- Musketeers/ScoreScene.swift
+++ Musketeers/ScoreScene.swift
@@ -11,31 +11,18 @@ import SpriteKit
 class ScoreScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        
-        self.addChild(myLabel)
+
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        var mainViewController = appDelegate.window!.rootViewController as? GameViewController
+        mainViewController!.showGameScene()
+        
         for touch: AnyObject in touches {
             let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
         }
     }
     
