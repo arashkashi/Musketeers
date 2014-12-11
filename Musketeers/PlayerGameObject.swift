1c93220eb01b4605697d3d0565fda03b50b03f1a
Arash Kashi <arashkashi@gmail.com>
2014-12-11 10:13:13 +0100

    Now the game player updates its travelled distance and has acceleration feature.

--- Musketeers/PlayerGameObject.swift
+++ Musketeers/PlayerGameObject.swift
@@ -11,6 +11,8 @@ import SpriteKit
 
 class PlayerGameObject: GameObject {
     var speed: CGFloat = 0.0001
+    var acceleration: CGFloat = 0.0
+    var distancetravelled: CGFloat = 0.0
     
     override init(node: SKNode?) {
         super.init(node: node)
@@ -21,8 +23,15 @@ class PlayerGameObject: GameObject {
         node!.physicsBody?.categoryBitMask = ENEMY
         node!.physicsBody?.collisionBitMask = PLAYER
         node!.physicsBody?.categoryBitMask = 0
-        
-        
     }
     
+    override func update(dt: Double, allObject: [GameObject]) {
+        super.update(dt, allObject: allObject)
+        
+        // Update distance
+        distancetravelled = speed * CGFloat(self.dt) + distancetravelled
+        
+        // Update speed based on Acc.
+        speed = acceleration * CGFloat(self.dt) + speed
+    }
 }
