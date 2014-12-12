a88fa3200ba1d25265f20720cdcae41453dc0034
Marko Kecman <marko.kecman@wooga.net>
2014-12-12 15:12:30 +0100

    Added enemy animation definitions;

--- Musketeers/EnemyGameObject.swift
+++ Musketeers/EnemyGameObject.swift
@@ -11,6 +11,12 @@ import SpriteKit
 
 class EnemyGameObject: GameObject {
     
+    var enemy : SKSpriteNode! = SKSpriteNode(imageNamed: "enemy1-0001");
+    var kill : SKSpriteNode! = SKSpriteNode(imageNamed: "enemy1kill-0001");
+    
+    var enemyAction: SKAction!
+    var killAction: SKAction!
+    
     override init(node: SKNode?) {
         super.init(node: node)
         type = .Enemy
@@ -20,6 +26,56 @@ class EnemyGameObject: GameObject {
 //        node!.physicsBody?.categoryBitMask = PLAYER
 //        node!.physicsBody?.collisionBitMask = ENEMY
 //        node!.physicsBody?.categoryBitMask = 0
+        
+        
+        
+        ///Standing
+        node!.addChild(enemy);
+        enemy.alpha = 0
+        var animArray = [SKTexture]();
+        for var index = 1; index < 40; index++
+        {
+            var prefix : String = "";
+            if ( index < 10 ) { prefix = "0"; }
+            var imageName : String = "enemy1-00" + prefix + String( index );
+            animArray.append( SKTexture(imageNamed: imageName))
+        }
+        let toad_hit1_anim = SKAction.animateWithTextures( animArray, timePerFrame: 0.06 )
+        self.enemyAction = toad_hit1_anim
+        
+        ///Standing
+        node!.addChild(kill);
+        kill.alpha = 0
+        animArray = [SKTexture]();
+        for var index = 1; index < 20; index++
+        {
+            var prefix : String = "";
+            if ( index < 10 ) { prefix = "0"; }
+            var imageName : String = "enemy1kill-00" + prefix + String( index );
+            animArray.append( SKTexture(imageNamed: imageName))
+        }
+        let toad_hit1_anim2 = SKAction.animateWithTextures( animArray, timePerFrame: 0.06 )
+        self.killAction = toad_hit1_anim2
+        
+        showEnemy();
+    }
+    
+    func showEnemy() {
+        enemy.alpha = 1
+        kill.alpha = 0
+        
+        //if enemy.actionForKey("running") == nil {
+            enemy.runAction( self.enemyAction, withKey: "running");
+        //}
+    }
+    
+    func showKill() {
+        enemy.alpha = 0
+        kill.alpha = 1
+        
+        if kill.actionForKey("running") == nil {
+            kill.runAction( self.killAction, withKey: "running");
+        }
     }
     
     override func update(dt: Double, allObject: [GameObject]) {
